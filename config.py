d99bf1d28cdafe6997e8cf7fc0cf4aebe7478597
gabriel <gabvieira1994@hotmail.com>
2021-01-23 19:28:31 +0000

    Added file configurations for metrics and logs servers

---
+++ config.py
@@ -0,0 +1,9 @@
+config_logs = {
+    "host": "127.0.0.1",
+    "port": 5000
+}
+
+config_metrics = {
+    "host": "127.0.0.1",
+    "port": 9102
+}
---
+++ main.py
@@ -0,0 +1,40 @@
+import threading
+
+from flask import Flask, jsonify
+import config
+
+#app = Flask(__name__)
+app1 = Flask(__name__)
+app2 = Flask(__name__)
+
+
+# def index() -> str:
+#     # transform a dict into an application/json response
+#     return jsonify({"message": "It Works"})
+
+@app1.route('/')
+def index1():
+    return 'Hello World 1'
+
+@app2.route('/')
+def index2():
+    return 'Hello World 2'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=900)
+
+# With Multi-Threading Apps, YOU CANNOT USE DEBUG!
+# Though you can sub-thread.
+def runLogs():
+    app1.run(host=config.config_logs["host"], port=config.config_logs["port"], debug=False, threaded=True)
+
+def runMetrics():
+    app2.run(host=config.config_metrics["host"], port=config.config_metrics["port"], debug=False, threaded=True)
+
+
+if __name__ == '__main__':
+    # Executing the Threads seperatly.
+    t1 = threading.Thread(target=runLogs)
+    t2 = threading.Thread(target=runMetrics)
+    t1.start()
+    t2.start()
